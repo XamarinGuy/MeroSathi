730699667f0f66202f0b5bb15d6afc69f7c058df
XamarinGuy <saurav101>
2018-05-13 07:53:40 +0530

    NewCommitLoveLove

--- MeroSathi/App.xaml.cs
+++ MeroSathi/App.xaml.cs
@@ -1,6 +1,9 @@
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace MeroSathi
@@ -16,7 +19,8 @@ namespace MeroSathi
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+           
+			AppCenter.Start("android=8d1a85a6-8b7a-49be-8e7e-3f26a2a0e199;" + "uwp={Your UWP App secret here};" + "ios={Your iOS App secret here}", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
